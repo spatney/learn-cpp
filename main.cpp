b4a35f7db8cd884c1eb1d481e7ab56a7153f0cfe
Sachin Patney <spatney@microsoft.com>
2019-01-21 20:06:40 -0800

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string message = "Hello World Again";
+    cout << message << endl;
+    
+    auto number = 10;
+
+    cout << to_string(10) + " is a Number" << endl;
+    return 0;
+}
