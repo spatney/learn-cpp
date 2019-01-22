effa869b8e37f1b51d35bf63076cff626796791a
Sachin Patney <spatney@microsoft.com>
2019-01-21 20:46:56 -0800

    update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
-int main() {
-    string message = "Hello World Again";
-    cout << message << endl;
-    
-    auto number = 10;
 
-    cout << to_string(10) + " is a Number" << endl;
+int main(int argc, char **argv)
+{
+    auto number(10);
+    auto message(" is the magic number.");
+
+    cout << to_string(number) + message << endl;
     return 0;
 }
